95ea54dd7abf8de9235be381de1af080c6a24d91
LINDBERG LUKA p2201643 <luka.lindberg@etu.univ-lyon1.fr>
2025-02-24 17:05:12 +0100

    discord command added

--- src/commands/discord.ts
+++ src/commands/discord.ts
@@ -1,5 +1,5 @@
 import { ChatUserstate, Client } from "tmi.js";
 
 export default function (client: Client, channel: string, tags: ChatUserstate, args: string[]) {
-    client.say(channel, `@${tags.username}, salut ! 👋`);
+    client.say(channel, `@${tags.username}, tiens voici le meilleur discord : https://discord.gg/PyuCs7aJwh`);
 }
